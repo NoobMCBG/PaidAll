153cd610da6719518127fa5e47236c8dc6fd32e0
Noob MCBG <84442886+NoobMCBG@users.noreply.github.com>
2022-01-26 10:41:52 +0700

    Add files via upload

---
+++ src/NoobMCBG/PayAll/PayAll.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NoobMCBG\PayAll;
+
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use onebone\economyapi\EconomyAPI;
+use NoobMCBG\PayAll\commands\PayAllCommands;
+
+class PayAll extends PluginBase implements Listener {
+
+	public static $instance;
+
+	public static function getInstance() : self {
+		return self::$instance;
+	}
+
+	public function onEnable() : void {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->saveDefaultConfig();
+		$this->getServer()->getCommandMap()->register("PayAll", new PayAllCommands($this));
+		self::$instance = $this;
+	}
+
+	public function payAll($money){
+		if(is_numeric($money)){
+            foreach($this->getServer()->getOnlinePlayers() as $player){
+            	if($player instanceof Player){
+            		$count = count($this->getServer()->getOnlinePlayers());
+            		$amount = $money/$count;
+            		EconomyAPI::getInstance()->addMoney($player, $amount);
+            	}
+            }
+        }
+	}
+}
