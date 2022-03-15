6376b81c766830adddf6d5b91f31a9450746caad
Noob MCBG <84442886+NoobMCBG@users.noreply.github.com>
2022-03-15 11:15:23 +0700

    Update Economy.php

--- src/NoobMCBG/PaidAll/Economy.php
+++ src/NoobMCBG/PaidAll/Economy.php
@@ -5,35 +5,36 @@ declare(strict_types=1);
 namespace NoobMCBG\PaidAll;
 
 use pocketmine\player\Player;
-use pocketmine\Server;
+use cooldogedev\BedrockEconomy\api\BedrockEconomyAPI;
+use onebone\economyapi\EconomyAPI;
 use NoobMCBG\PaidAll\PaidAll;
 
 class Economy {
 
     public static function addMoney(Player $player, int $amount){
         if(PaidAll::getInstance()->getDefaultCurrencyUnit()["BedrockEconomy"] == true){
-            Server::getInstance()->getPluginManager()->getPlugin("BedrockEconomy")->getAPI()->addToPlayerBalance($player->getName(), (int)$amount);
+            BedrockEconomyAPI::getInstance()->addToPlayerBalance($player->getName(), (int)$amount);
         }
         if(PaidAll::getInstance()->getDefaultCurrencyUnit()["EconomyAPI"] == true){
-            Server::getInstance()->getPluginManager()->getPlugin("EconomyAPI")->addMoney($player, (int)$amount);
+            EconomyAPI::getInstance()->addMoney($player, (int)$amount);
         }
     }
 
     public static function reduceMoney(Player $player, int $amount){
         if(PaidAll::getInstance()->getDefaultCurrencyUnit()["BedrockEconomy"] == true){
-            Server::getInstance()->getPluginManager()->getPlugin("BedrockEconomy")->getAPI()->subtractFromPlayerBalance($player->getName(), (int)$amount);
+            BedrockEconomyAPI::getInstance()->subtractFromPlayerBalance($player->getName(), (int)$amount);
         }
         if(PaidAll::getInstance()->getDefaultCurrencyUnit()["EconomyAPI"] == true){
-            Server::getInstance()->getPluginManager()->getPlugin("EconomyAPI")->reduceMoney($player, (int)$amount);
+            EconomyAPI::getInstance()->reduceMoney($player, (int)$amount);
         }
     }
 
     public static function myMoney(Player $player){
         if(PaidAll::getInstance()->getDefaultCurrencyUnit()["BedrockEconomy"] == true){
-            return Server::getInstance()->getPluginManager()->getPlugin("BedrockEconomy")->getAPI()->getPlayerBalance($player->getName());
+            return BedrockEconomyAPI::getInstance()->getPlayerBalance($player->getName());
         }
         if(PaidAll::getInstance()->getDefaultCurrencyUnit()["EconomyAPI"] == true){
-            return Server::getInstance()->getPluginManager()->getPlugin("EconomyAPI")->myMoney($player);
+            return EconomyAPI::getInstance()->myMoney($player);
         }
     }
 }
