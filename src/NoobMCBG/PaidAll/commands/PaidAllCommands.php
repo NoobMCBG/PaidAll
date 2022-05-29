2ce2da12072ab0dd305168262a8c8dd77d74cb94
Noob MCBG <84442886+NoobMCBG@users.noreply.github.com>
2022-05-29 12:07:57 +0700

    Update PaidAllCommands.php

--- src/NoobMCBG/PaidAll/commands/PaidAllCommands.php
+++ src/NoobMCBG/PaidAll/commands/PaidAllCommands.php
@@ -24,54 +24,62 @@ class PaidAllCommands extends Command implements PluginOwned {
 	public function execute(CommandSender $sender, string $label, array $args){
 		if(!$sender instanceof Player){
 			if(isset($args[0])){
-                if(!is_numeric($args[0])){
-		        	$sender->sendMessage("§cUsage:§7 /paidall <money>");
-		        	return true;
-		        }
-		        if(count($this->getOwningPlugin()->getServer()->getOnlinePlayers()) == 0){
-		        	$sender->sendMessage("There is 0 players online !");
-		        }else{
-		            $money = (int)$args[0];
-                    $count = count($this->getOwningPlugin()->getServer()->getOnlinePlayers());
-		        	$amount = (int)$args[0]/$count;
-		            $this->getOwningPlugin()->paidAll($money);
-		            if($this->getOwningPlugin()->getConfig()->getAll()["broadcast-paid"]["broadcast"] == true){
-                        $this->getOwningPlugin()->getServer()->broadcastMessage(str_replace(["{money}", "{player}"], [$amount, "CONSOLE"], strval($this->getOwningPlugin()->getConfig()->getAll()["broadcast-paid"]["msg-broadcast-paid"])));
-                    }
-		        }
-		    }else{
-		    	$sender->sendMessage("§cUsage:§7 /paidall <money>");
-		    }
+                		if(!is_numeric($args[0])){
+		        		$sender->sendMessage("§cUsage:§7 /paidall <money>");
+		        		return true;
+		        	}
+		        	if(count($this->getOwningPlugin()->getServer()->getOnlinePlayers()) == 0){
+		        		$sender->sendMessage("There is 0 players online !");
+		        	}else{
+		            		$money = (int)$args[0];
+                    			$count = count($this->getOwningPlugin()->getServer()->getOnlinePlayers());
+		        		$amount = (int)$args[0]/$count;
+					if($money < $min = $this->getOwningPlugin()->getConfig()->get("min-paid")){
+						$sender->sendMessage("Amount paid is not less than $min");
+						return true;
+					}
+		            		$this->getOwningPlugin()->paidAll($money);
+		            		if($this->getOwningPlugin()->getConfig()->getAll()["broadcast-paid"]["broadcast"] == true){
+                        			$this->getOwningPlugin()->getServer()->broadcastMessage(str_replace(["{money}", "{player}"], [$amount, "CONSOLE"], strval($this->getOwningPlugin()->getConfig()->getAll()["broadcast-paid"]["msg-broadcast-paid"])));
+                    			}
+		        	}
+		   	}else{
+		    		$sender->sendMessage("§cUsage:§7 /paidall <money>");
+		   	}
 		}else{
 			if(isset($args[0])){
-		        if(!is_numeric($args[0])){
-		        	$sender->sendMessage("§cUsage:§7 /paidall <money>");
-		        	return true;
-		        }
-		        foreach($this->getOwningPlugin()->getServer()->getOnlinePlayers() as $player){
-		        	if($player instanceof Player){
-		        		if(Economy::myMoney($sender) >= (int)$args[0]){
-		        		    $count = count($this->getOwningPlugin()->getServer()->getOnlinePlayers());
-		        		    $amount = (int)$args[0]/$count;
-                            Economy::reduceMoney($sender, $args[0]);
-                            Economy::addMoney($player, $amount);
-                            if($this->getOwningPlugin()->getConfig()->getAll()["paid-successfully"]["msg"] == true){
-                                $sender->sendMessage(str_replace(["{money}"], [$amount], strval($this->getOwningPlugin()->getConfig()->getAll()["paid-successfully"]["msg-paid-successfully"])));
-                            }
-                            if($this->getOwningPlugin()->getConfig()->getAll()["broadcast-paid"]["broadcast"] == true){
-                                $this->getOwningPlugin()->getServer()->broadcastMessage(str_replace(["{money}", "{player}"], [$amount, $sender->getName()], strval($this->getOwningPlugin()->getConfig()->getAll()["broadcast-paid"]["msg-broadcast-paid"])));
-                            }
-                        }else{
-                        	$price = (int)$args[0] - Economy::myMoney($sender);
-                        	if($this->getOwningPlugin()->getConfig()->getAll()["paid-fallied"]["msg"] == true){
-                                $sender->sendMessage(str_replace(["{price}"], [$price], strval($this->getOwningPlugin()->getConfig()->getAll()["paid-fallied"]["msg-paid-fallied"])));
-                            }
-                        }
+		        	if(!is_numeric($args[0])){
+		        		$sender->sendMessage("§cUsage:§7 /paidall <money>");
+		        		return true;
 		        	}
-		        }
-		    }else{
-		    	$sender->sendMessage("§cUsage:§7 /paidall <money>");
-		    }
+				if((int)$args[0] < $min = $this->getOwningPlugin()->getConfig()->get("min-paid")){
+					$sender->sendMessage("Amount paid is not less than $min");
+					return true;
+				}
+		        	foreach($this->getOwningPlugin()->getServer()->getOnlinePlayers() as $player){
+		        		if($player instanceof Player){
+		        			if(Economy::myMoney($sender) >= (int)$args[0]){
+		        		    		$count = count($this->getOwningPlugin()->getServer()->getOnlinePlayers());
+		        		    		$amount = (int)$args[0]/$count;
+                            				Economy::reduceMoney($sender, $args[0]);
+                            				Economy::addMoney($player, $amount);
+                            				if($this->getOwningPlugin()->getConfig()->getAll()["paid-successfully"]["msg"] == true){
+                                				$sender->sendMessage(str_replace(["{money}"], [$amount], strval($this->getOwningPlugin()->getConfig()->getAll()["paid-successfully"]["msg-paid-successfully"])));
+                            				}
+                            				if($this->getOwningPlugin()->getConfig()->getAll()["broadcast-paid"]["broadcast"] == true){
+                                				$this->getOwningPlugin()->getServer()->broadcastMessage(str_replace(["{money}", "{player}"], [$amount, $sender->getName()], strval($this->getOwningPlugin()->getConfig()->getAll()["broadcast-paid"]["msg-broadcast-paid"])));
+                            				}
+                        			}else{
+                        				$price = (int)$args[0] - Economy::myMoney($sender);
+                        				if($this->getOwningPlugin()->getConfig()->getAll()["paid-fallied"]["msg"] == true){
+                                				$sender->sendMessage(str_replace(["{price}"], [$price], strval($this->getOwningPlugin()->getConfig()->getAll()["paid-fallied"]["msg-paid-fallied"])));
+                            				}
+                       				}
+		        		}
+		        	}
+		    	}else{
+		    		$sender->sendMessage("§cUsage:§7 /paidall <money>");
+		    	}
 		}
 	}
 
